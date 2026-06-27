6dddea451cab1cd056dac1338127eb24ee63a3de
MrOneD <98280063+MrOneD@users.noreply.github.com>
2026-06-27 13:34:04 +0200

    rrrty

--- Ritual.cs
+++ Ritual.cs
@@ -23,7 +23,7 @@ namespace OriathHub.Plugins.Ritual
         public override string Name => "Ritual";
         public override string Author => "MrOne";
         public override string Description => "Pricer for Ritual Rewards";
-        public override string Version => "0.3.22";
+        public override string Version => "0.3.222";
 
         public override void OnEnable(bool isGameOpened)
         {
